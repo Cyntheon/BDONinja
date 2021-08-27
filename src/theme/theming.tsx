ea00feb2b8c7fe8ac28fa41ad697e2b787543b4e
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-27 07:52:43 -0400

    Add theming

---
+++ src/theme/theming.tsx
@@ -0,0 +1,23 @@
+import {createTheme} from "@material-ui/core";
+import {pink} from "@material-ui/core/colors";
+import {ThemeProvider} from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: pink["800"]
+    },
+    secondary: {
+      main: pink["A100"]
+    }
+  }
+});
+
+export const CustomThemeProvider = ({children}: {children: any}) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );
+};
