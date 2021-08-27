17650d0ae09e56600624c76be16af0f6d53d754e
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-27 08:57:55 -0400

    Simplify start page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import {Button} from "@material-ui/core";
+import {css} from "@emotion/react";
 
 const Index = () => {
   return (
-    <Button
-      size="large"
-      color="secondary"
-    >
-      Get Started
-    </Button>
+    <div>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        css={css`
+        margin: 0 auto;
+      `}
+      >
+        Get Started
+      </Button>
+    </div>
   );
 };
 
