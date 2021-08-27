c7c85722868977c1f5cabba11d61b1570f9e8907
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-27 06:37:42 -0400

    Add support for emotion css-in-js and css prop

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import {css} from "@emotion/react";
+
+const Index = () => {
+  return (
+    <div
+      css={css`
+        background-color: aqua;    
+      `}
+    >
+      Testing
+    </div>
+  );
+};
+
+export default Index;
