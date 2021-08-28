1fcd485caae11c3ac8d45aea85c3f2dbfcfd6a36
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-28 16:32:39 -0400

    Edit index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import {Button} from "@material-ui/core";
+import {Button, Typography} from "@material-ui/core";
 import {css} from "@emotion/react";
 
 const Index = () => {
   return (
     <div>
+      <Typography variant="h1">
+        Welcome to BDO Ninja
+      </Typography>
+
       <Button
         variant="contained"
         color="secondary"
         size="large"
         css={css`
-        margin: 0 auto;
-      `}
+          margin: 0 auto;
+        `}
       >
         Get Started
       </Button>
