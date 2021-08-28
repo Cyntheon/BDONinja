af238e08d983ee9d1bada0bac2fd02ba6fed1d16
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-28 16:30:32 -0400

    Move global app styling logic to separate component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,9 @@
 import {AppProps} from "next/app";
 import Head from "next/head";
-import {useEffect} from "react";
-import {CssBaseline, MuiThemeProvider} from "@material-ui/core";
-import {Global} from "@emotion/react";
-import theme from "style/theme";
-import reset from "style/reset";
 import Main from "main/Main";
+import GlobalStyling from "style/GlobalStyling";
 
 const CustomApp = ({Component, pageProps}: AppProps) => {
-  useEffect(() => {
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <>
       <Head>
@@ -24,13 +13,11 @@ const CustomApp = ({Component, pageProps}: AppProps) => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Global styles={reset} />
+      <GlobalStyling>
         <Main>
           <Component {...pageProps} />
         </Main>
-      </MuiThemeProvider>
+      </GlobalStyling>
     </>
   );
 };
