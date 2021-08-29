d85c4a5624d07a5fc44dd57089f65c113afb707b
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-29 04:09:02 -0400

    Add banner image to card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import {
   Button,
   CardActionArea, CardActions,
-  CardContent, Icon, IconButton,
+  CardContent, CardMedia, Icon, IconButton,
   Link as MuiLink,
   Paper,
   Typography
@@ -9,6 +9,7 @@ import {
 import Link from "next/link";
 import {NextSeo} from "next-seo";
 import {Share, Timer} from "@material-ui/icons";
+import {css} from "@emotion/react";
 
 const Index = () => {
   return (
@@ -19,15 +20,38 @@ const Index = () => {
           title: "Home"
         }}
       />
+      <Typography
+        variant="h3"
+        component="h1"
+        css={css`
+          &.MuiTypography-root {
+            text-align: center;
+            margin-bottom: 32px;
+          }
+        `}
+      >
+        Welcome to BDO Ninja!
+      </Typography>
       <Paper elevation={3}>
         <CardActionArea>
+          <CardMedia
+            component="img"
+            alt=""
+            height="250"
+            image="https://live.staticflickr.com/4758/39260746395_6a32e97eb8_b.jpg"
+            title="Cooking Time Calculator"
+          />
           <CardContent>
-            <Icon>
-              <Timer color="primary" />
-            </Icon>
             <Link href="/cooking/time" passHref>
               <MuiLink href="" color="primary">
                 <Typography variant="h4" component="h2">
+                  <Icon
+                    css={css`
+                      margin-right: 8px;
+                    `}
+                  >
+                    <Timer color="primary" />
+                  </Icon>
                   Cooking Time Calculator
                 </Typography>
               </MuiLink>
