ae128eea2f2d6b10a63a7c235deb5ba2c1878824
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-29 00:51:52 -0400

    Redirect from index to /cooking/cookTime

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/cooking/cookTime",
+      permanent: true
+    }
+  ]
+};
