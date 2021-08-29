a6dec61356b762dbf9ecf2196de67f1683302bb5
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-29 19:12:47 -0400

    Temporarily disable card image

--- src/navCard/NavCardBanner.tsx
+++ src/navCard/NavCardBanner.tsx
@@ -1,6 +1,4 @@
-import {CardMedia} from "@material-ui/core";
-import React, {useEffect, useLayoutEffect} from "react";
-import Image from "next/image";
+import React from "react";
 
 interface Props {
   title: string;
@@ -8,25 +6,18 @@ interface Props {
 }
 
 const NavCardBanner = ({title, image}: Props) => {
-  useEffect(() => {
-    console.log("Effect fire!");
-  }, []);
-
-  useLayoutEffect(() => {
-    console.log("Layout Effect fire!");
-  }, []);
-
   return (
-    <CardMedia>
-      <Image
-        src={image}
-        height={288}
-        width={512}
-        title={title}
-        alt=""
-        placeholder="blur"
-      />
-    </CardMedia>
+    // <CardMedia>
+    //   <Image
+    //     src={image}
+    //     height={288}
+    //     width={512}
+    //     title={title}
+    //     alt=""
+    //     placeholder="blur"
+    //   />
+    // </CardMedia>
+    <> </>
   );
 };
 
