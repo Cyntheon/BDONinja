4f73ff0191c08c3684c69c29f4743b101db166fd
Cynthia Chung <cynthiachung03@gmail.com>
2021-08-30 02:30:33 -0400

    Rename font

--- src/style/font.tsx
+++ src/style/font.tsx
@@ -2,14 +2,14 @@ import {css} from "@emotion/react";
 
 const font = css`
   @font-face {
-    font-family: "StrongSword_Pure";
+    font-family: "BlackSpirit";
     font-weight: normal;
     font-style: normal;
     font-display: swap;
 
-    src: url('https://s1.pearlcdn.com/global_static/font/web/web_pearl_original.woff2') format('woff2'),
-         url('https://s1.pearlcdn.com/global_static/font/web/web_pearl_original.woff') format('woff'),
-         url('https://s1.pearlcdn.com/global_static/font/web/web_pearl_original.ttf') format('truetype');
+    src: url("https://s1.pearlcdn.com/global_static/font/web/web_pearl_original.woff2") format("woff2"),
+         url("https://s1.pearlcdn.com/global_static/font/web/web_pearl_original.woff") format("woff"),
+         url("https://s1.pearlcdn.com/global_static/font/web/web_pearl_original.ttf") format("truetype");
   }
 `;
 
